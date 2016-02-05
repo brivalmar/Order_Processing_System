bedf5c81a5d45c682ec303d24851582cde957bf3
Briley Marchetti <brimarchetti@gmail.com>
2016-02-04 19:35:18 -0500

    Some threading stuff...learning. This is nothing of quality.

--- MainController.java
+++ MainController.java
@@ -7,27 +7,7 @@ import java.util.ArrayList;
 public class MainController{
     public static void main(String[] args){
 
-        // Call methods to read text files of items and customers
-        ArrayList<InventoryItem> itemList = InventoryController.readItemList();
-        ArrayList<Customer> customerList = CustomerController.readCustomerList();
-
-        // Tests an array of orders...
-        int totalTests = 100;
-        for(int i = 0; i < totalTests; i++){
-            int randNum1 = (int) Math.floor(Math.random() * itemList.size());
-            int randNum2 = (int) Math.floor(Math.random() * customerList.size());
-            int randNum3 = (int) Math.floor((Math.random() * 6) + 1);
-
-            Order[] orders = new Order[totalTests];
-            orders[i] = new Order(itemList.get(randNum1), customerList.get(randNum2), (i + 1), randNum3);
-            orders[i].checkAndSubtractInventoryItemQuantity(orders[i].getInventoryItem());
-        }
-
-        // Testing Returns and incrementing itemQuantity
-        // Return return1 = new Return(itemList.get(1), customerList.get(1), 1, 1);
-        // return1.addToInventoryItemQuantity(return1.getInventoryItem());
-
-        // Writes out itemList with new quanitties if adjusted
-        InventoryController.writeOutInventoryItems(itemList);
+        Thread t1 = new Thread(new RunThread());
+        Thread t2 = new Thread(new RunThread());
     }
 }
